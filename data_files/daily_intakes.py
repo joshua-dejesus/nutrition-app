636fad333d3dbaa4d78b010055d55c5858609b6e
Joshua DeJesus <joshuadejesus007@gmail.com>
2026-01-21 19:44:43 -0500

    Changed data structure for better formatting

--- data_files/daily_intakes.py
+++ data_files/daily_intakes.py
@@ -1,3 +1,4 @@
+from typing import Any, OrderedDict
 import camelot
 
 tables = camelot.read_pdf('data_files/amdr_table.pdf', flavor='stream')
@@ -5,32 +6,43 @@ df = tables[0].df
 
 def get_nutrition_recommendation(age_in_years: int, sex: str=None):
 
+    col1, col2 = df[0], None
+
     if sex == None and age_in_years in range(1, 4):
-        return str(df[0] + "            " + df[2])
+        col2 = df[2]
     
     if sex == 'male':
         if age_in_years in range(4, 9):
-            return str(df[0] + "            " + df[4])
+            col2 = df[4]
         elif age_in_years in range(9, 14):
-            return str(df[0] + "            " + df[6])
+            col2 = df[6]
         elif age_in_years in range(14, 19):
-            return str(df[0] + "            " + df[8])
+            col2 = df[8]
         elif age_in_years in range (19, 31):
-            return str(df[0] + "            " + df[10])
+            col2 = df[10]
         elif age_in_years in range(31, 50):
-            return str(df[0] + "            " + df[12])
+            col2 = df[12]
         else:
-            return str(df[0] + "            " + df[14])
+            col2 = df[14]
     else:
         if age_in_years in range(4, 9):
-            return str(df[0] + "            " + df[3])
+            col2 = df[3]
         elif age_in_years in range(9, 14):
-            return str(df[0] + "            " + df[5])
+            col2 = df[5]
         elif age_in_years in range(14, 19):
-            return str(df[0] + "            " + df[7])
+            col2 = df[7]
         elif age_in_years in range (19, 31):
-            return str(df[0] + "            " + df[9])
+            col2 = df[9]
         elif age_in_years in range(31, 50):
-            return str(df[0] + "            " + df[11])
+            col2 = df[11]
         else:
-            return str(df[0] + "            " + df[13])
+            col2 = df[13]
+
+    if col2 is not None:
+        nutrients_dict = OrderedDict[Any, Any]()
+        for i in range (len(col1)):
+            nutrient_name = str(col1.iloc[i]).strip()
+            nutrient_value = str(col2.iloc[i]).strip()
+            nutrients_dict[nutrient_name] = nutrient_value
+
+    return nutrients_dict
